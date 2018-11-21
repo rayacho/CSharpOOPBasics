dab4726b3d96deaa9b7c85a75a44e219160ae91a
rayacho <shawnsgf@abv.bg>
2018-11-21 10:07:50 +0200

    added WorkingWithAbstraction.sln

---
+++ WorkingWithAbstraction/3.JediGalaxy/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.JediGalaxy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] dimensions = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+			int x = dimensions[0];
+			int y = dimensions[1];
+
+			int[,] matrix = new int[x, y];
+
+			int value = 0;
+			for (int i = 0; i < x; i++)
+			{
+				for (int j = 0; j < y; j++)
+				{
+					matrix[i, j] = value++;
+				}
+			}
+
+			string command = Console.ReadLine();
+			long sum = 0;
+			while (command != "Let the Force be with you")
+			{
+				int[] ivoCoordinates = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+				int[] evilCoordinates = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+				int xEvil = evilCoordinates[0];
+				int yEvil = evilCoordinates[1];
+
+				while (xEvil >= 0 && yEvil >= 0)
+				{
+					if (xEvil >= 0 && xEvil < matrix.GetLength(0) && yEvil >= 0 && yEvil < matrix.GetLength(1))
+					{
+						matrix[xEvil, yEvil] = 0;
+					}
+					xEvil--;
+					yEvil--;
+				}
+
+				int xIvo = ivoCoordinates[0];
+				int yIvo = ivoCoordinates[1];
+
+				while (xIvo >= 0 && yIvo < matrix.GetLength(1))
+				{
+					if (xIvo >= 0 && xIvo < matrix.GetLength(0) && yIvo >= 0 && yIvo < matrix.GetLength(1))
+					{
+						sum += matrix[xIvo, yIvo];
+					}
+
+					yIvo++;
+					xIvo--;
+				}
+
+				command = Console.ReadLine();
+			}
+
+			Console.WriteLine(sum);
+		}
+	}
+}
---
+++ WorkingWithAbstraction/5.GreedyTimes/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.GreedyTimes
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			long bagCapacity = long.Parse(Console.ReadLine());
+			string[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+			var bag = new Dictionary<string, Dictionary<string, long>>();
+			long gold = 0;
+			long gem = 0;
+			long cash = 0;
+
+			for (int i = 0; i < input.Length; i += 2)
+			{
+				string name = input[i];
+				long count = long.Parse(input[i + 1]);
+
+				string type = string.Empty;
+
+				if (name.Length == 3)
+				{
+					type = "Cash";
+				}
+				else if (name.ToLower().EndsWith("gem"))
+				{
+					type = "Gem";
+				}
+				else if (name.ToLower() == "gold")
+				{
+					type = "Gold";
+				}
+
+				if (type == "")
+				{
+					continue;
+				}
+				else if (bagCapacity < bag.Values.Select(x => x.Values.Sum()).Sum() + count)
+				{
+					continue;
+				}
+
+				switch (type)
+				{
+					case "Gem":
+						if (!bag.ContainsKey(type))
+						{
+							if (bag.ContainsKey("Gold"))
+							{
+								if (count > bag["Gold"].Values.Sum())
+								{
+									continue;
+								}
+							}
+							else
+							{
+								continue;
+							}
+						}
+						else if (bag[type].Values.Sum() + count > bag["Gold"].Values.Sum())
+						{
+							continue;
+						}
+						break;
+					case "Cash":
+						if (!bag.ContainsKey(type))
+						{
+							if (bag.ContainsKey("Gem"))
+							{
+								if (count > bag["Gem"].Values.Sum())
+								{
+									continue;
+								}
+							}
+							else
+							{
+								continue;
+							}
+						}
+						else if (bag[type].Values.Sum() + count > bag["Gem"].Values.Sum())
+						{
+							continue;
+						}
+						break;
+				}
+
+				if (!bag.ContainsKey(type))
+				{
+					bag[type] = new Dictionary<string, long>();
+				}
+
+				if (!bag[type].ContainsKey(name))
+				{
+					bag[type][name] = 0;
+				}
+
+				bag[type][name] += count;
+				if (type == "Gold")
+				{
+					gold += count;
+				}
+				else if (type == "Gem")
+				{
+					gem += count;
+				}
+				else if (type == "Cash")
+				{
+					cash += count;
+				}
+			}
+
+			foreach (var x in bag)
+			{
+				Console.WriteLine($"<{x.Key}> ${x.Value.Values.Sum()}");
+				foreach (var item in x.Value.OrderByDescending(y => y.Key).ThenBy(y => y.Value))
+				{
+					Console.WriteLine($"##{item.Key} - {item.Value}");
+				}
+			}
+		}
+	}
+}
