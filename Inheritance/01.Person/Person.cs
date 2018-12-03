fa662ce98ad5c53f5cfd9a0ce60d9ca333e1db81
rayacho <shawnsgf@abv.bg>
2018-12-03 11:34:06 +0200

    added ex.1,2

---
+++ Inheritance/01.Person/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Person
+{
+	public class Person
+	{
+		public const int minLength = 3;
+		private string _name;
+		private int _age;
+
+		public virtual string Name
+		{
+			get { return _name; }
+			set
+			{
+				if(value.Length < 3)
+				{
+					throw new ArgumentException($"Name's length should not be less than {minLength} symbols!");
+				}
+				_name = value;
+			}
+		}
+
+		public virtual int Age
+		{
+			get { return _age; }
+			set
+			{
+				if(value < 0)
+				{
+					throw new ArgumentException("Age must be positive!");
+				}
+				_age = value;
+			}
+		}
+
+		public Person(string name, int age)
+		{
+			Name = name;
+			Age = age;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.Append(String.Format("Name: {0}, Age: {1}", Name, Age));
+
+			return stringBuilder.ToString();
+		}
+	}
+
+	public class Child : Person
+	{
+		private const int MaxAge = 15;
+		
+		public Child(string name, int age) : base(name, age)
+		{
+		}
+
+		public override int Age
+		{
+			get
+			{
+				return base.Age;
+			}
+			set
+			{ 
+				if (value > MaxAge)
+				{
+					throw new ArgumentException($"Child's age must be less than {MaxAge}!");
+				}
+
+				base.Age = value;
+			}
+		}
+	}
+}
+
---
+++ Inheritance/2.BookShop/Book.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2.BookShop
+{
+	public class Book
+	{
+		private string _title;
+		private string _author;
+		private double _price;
+
+		public Book(string author, string title, double price)
+		{
+			Author = author;
+			Title = title;
+			Price = price;
+		}
+
+		public string Title
+		{
+			get { return _title; }
+			set
+			{
+				if (value.Length < 3)
+				{
+					throw new ArgumentException("Title not valid!");
+				}
+				_title = value;
+			}
+		}
+		
+		public string Author
+		{
+			get { return _author; }
+			set
+			{
+				string[] authorNames = value.Split();
+
+				if (authorNames.Length == 2 && char.IsDigit(authorNames[1][0]))
+				{
+					throw new ArgumentException("Author not valid!");
+				}
+
+				_author = value;
+			}
+		}
+
+		public virtual double Price
+		{
+			get { return _price; }
+			set
+			{
+				if (value <= 0)
+				{
+					throw new ArgumentException("Price not valid!");
+				}
+
+				_price = value;
+			}
+		}
+
+		public override string ToString()
+		{
+			StringBuilder builder = new StringBuilder();
+			builder.AppendLine($"Type: {GetType().Name}")
+				.AppendLine($"Title: {Title}")
+				.AppendLine($"Author: {Author}")
+				.AppendLine($"Price: {Price:F2}");
+
+			string result = builder.ToString().TrimEnd();
+			return result;
+		}
+	}
+
+	public class GoldenEditionBook : Book
+	{
+		public GoldenEditionBook(string author, string title, double price)
+			: base(author, title, price) { }
+
+		public override double Price
+		{
+			get
+			{
+				return base.Price * 1.3;
+			}
+		}
+	}
+}
